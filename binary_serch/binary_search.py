3133fe5d0eebd6cb625e2b58cd08645e84a45cef
gillianomenezes <gilliano.menezes@loadsmart.com>
2022-12-02 11:32:43 -0300

    Add binary search algorithm

---
+++ binary_serch/binary_search.py
@@ -0,0 +1,15 @@
+from math import floor
+
+def binary_search(item_list, item):
+    start = 0
+    end = len(item_list)-1
+    while start <= end:
+        medium = floor((start + end)/2)
+        if item_list[medium] == item:
+            return medium
+        elif item < item_list[medium] :
+            end = medium -1
+        else:
+            start = medium + 1
+
+    return None
---
+++ binary_serch/test_binary_search.py
@@ -0,0 +1,12 @@
+import pytest 
+from binary_search import binary_search
+
+
+@pytest.mark.parametrize("item_list, item, expected_index", [
+    ([i for i in range(10)], 5, 5),
+    ([i for i in range(10)], 11, None),
+])
+def test_binary_search_find_element(item_list, item, expected_index):
+    item_index = binary_search(item_list, item)
+
+    assert item_index == expected_index
