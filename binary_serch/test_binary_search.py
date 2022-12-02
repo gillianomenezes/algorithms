0e6483ab38de662b6fd8f62ca02d8e48aaab3ec3
gillianomenezes <gilliano.menezes@loadsmart.com>
2022-12-02 11:33:49 -0300

    black .

--- binary_serch/test_binary_search.py
+++ binary_serch/test_binary_search.py
@@ -1,11 +1,14 @@
-import pytest 
+import pytest
 from binary_search import binary_search
 
 
-@pytest.mark.parametrize("item_list, item, expected_index", [
-    ([i for i in range(10)], 5, 5),
-    ([i for i in range(10)], 11, None),
-])
+@pytest.mark.parametrize(
+    "item_list, item, expected_index",
+    [
+        ([i for i in range(10)], 5, 5),
+        ([i for i in range(10)], 11, None),
+    ],
+)
 def test_binary_search_find_element(item_list, item, expected_index):
     item_index = binary_search(item_list, item)
 
