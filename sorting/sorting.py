6da818a7e4c3d1f9019db2e2958c04bb7d872fd8
gillianomenezes <gilliano.menezes@loadsmart.com>
2022-12-14 18:29:27 -0300

    add selection sorting

---
+++ sorting/sorting.py
@@ -0,0 +1,23 @@
+def get_lower_element_index(array):
+    lower = array[0]
+    lower_index = 0
+    for i in range(1,len(array)):
+        if lower > array[i]:
+            lower = array[i]
+            lower_index = i
+
+    return lower_index
+
+
+def selection_sorting(input_array):
+    if len(input_array) < 0:
+        return input_array 
+
+    array = input_array.copy()
+    sorted_array = []
+
+    for _ in range(len(array)):
+        lower_index = get_lower_element_index(array)
+        sorted_array.append(array.pop(lower_index))
+
+    return sorted_array
---
+++ sorting/test_sorting.py
@@ -0,0 +1,11 @@
+import pytest
+import random
+
+from sorting import selection_sorting
+
+@pytest.mark.parametrize("input_array", [([]), ([3,2,1]), (random.sample(range(10, 30), 5))])
+def test_selection_sorting(input_array):    
+    sorted_array = selection_sorting(input_array)
+    
+    input_array.sort()
+    assert sorted_array == input_array
